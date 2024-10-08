2fd5991f98a05233deee5c3f5fc5b784c3a28a63
LOTUR1 <120498689+LOTUR1@users.noreply.github.com>
2024-10-08 13:59:54 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,5 @@ public class Main {
     public static void main(String[] args) {
         Game game = new Game();
         game.gameStart();
-        Player player = game.user;
-        player.printCards();
-        System.out.println();
-        player.sortCards(game.deck, game.trump);
-        player.printCards();
     }
 }
